f131fa3033a60cabc8395095e0d4c7bebf000e12
Mattes D <github@xoft.cz>
2023-01-20 22:58:45 +0100

    Added hashing any string to the SofiaHash test program.
    This way it is easy to get the hash of any password for scripting purposes.

--- Tests/00-SofiaHash.cpp
+++ Tests/00-SofiaHash.cpp
@@ -19,8 +19,19 @@ static void testHash(const std::string & aOrigText, const std::string & aExpecte
 
 
 
-int main()
+/** This test program has a dual purpose:
+- it checks the NetSurveillancePp::sofiaHash() function against known hashes
+- it can hash any string (provided as the first commandline parameter) */
+int main(int aArgC, char * aArgV[])
 {
+	// If there is a commandline param, output the hash to stdout and exit:
+	if (aArgC > 1)
+	{
+		std::cout << NetSurveillancePp::sofiaHash(aArgV[1]) << "\n";
+		return 0;
+	}
+
+	// If there are no commandline params, check the hashes of known values:
 	try
 	{
 		testHash("admin",    "6QNMIQGe");
